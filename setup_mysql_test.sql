c397a5b57f7c31516f1127134fc47d8c2c32745a
Nesgomez9 <nicolico99@hotmail.com>
2020-04-01 07:03:45 +0000

    [Feature] Adding comment to the setup of MySQL

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,4 +1,4 @@
--- Prepare the Mysql Server;
+-- script that prepares a MySQL server for the project creating a a database to test 
 CREATE DATABASE IF NOT EXISTS hbnb_test_db;
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
