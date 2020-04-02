180bdc7dc8322c61c134586fdc8c694b39fff5b5
Derhks <derhks@gmail.com>
2020-04-02 18:50:33 +0000

    [Feature] check if hbnb_type_storage is equal to db

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """This is the user class"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
+import os
 
 
 class User(BaseModel, Base):
@@ -14,7 +15,14 @@ class User(BaseModel, Base):
     """
 
     __tablename__ = "users"
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
