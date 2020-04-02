550cc98ab25dcc4a79bcbdaafc058de3cf290516
Derhks <derhks@gmail.com>
2020-04-02 23:04:26 +0000

    [Update] class attribute

--- models/review.py
+++ models/review.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """This is the review class"""
-from models.base_model import BaseModel
+import models
+from os import environ
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, String, Integer, Float
+from sqlalchemy import ForeignKey, MetaData, Table
 from sqlalchemy.orm import relationship
 
 
@@ -15,6 +17,6 @@ class Review(BaseModel, Base):
     """
 
     __tablename__ = 'reviews'
+    place_id = Column(String(60), ForeignKey('places.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('user.id'), nullable=False)
     text = Column(String(1024), nullable=False)
-    place_id = Column(String(60), nullable=False, ForeignKey('places.id'))
-    user_id = Column(String(60), nullable=False, ForeignKey('users.id'))
