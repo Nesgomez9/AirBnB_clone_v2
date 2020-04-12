f706eb6363484930ecc29e8a38333d5320230867
Nesgomez9 <nicolico99@hotmail.com>
2020-04-12 18:21:16 +0000

    [Add] Adding task 2

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+from fabric.api import *
+from datetime import datetime
+import os
+
+env.hosts = ['34.74.237.63', '54.234.24.109']
+env.user = 'ubuntu'
+
+def do_deploy(archive_path):
+
+    try:
+        put(archive_path, "/tmp/")
+        run("mkdir -p /data/web_static/releases/")
+        f_file = os.path.basename(archive_path)
+        m_file = os.path.splitext(f_file)
+        run("tar -C  /data/web_static/releases/{} -xvf {}".format(m_file, f_file))
+        run("rm /tmp/{}".format(f_file))
+        run("rm -rf /data/web_static/current")
+        run("ln -sf /data/web_static/releases/{} /data/web_static/current".format(m_file))
+        return True
+    except:
+        return False
