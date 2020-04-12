4ef6ae344ea4e2d867ac2ed926e94380a9d6952d
Nesgomez9 <nicolico99@hotmail.com>
2020-04-12 19:15:34 +0000

    [Feature] Fixing task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -4,12 +4,9 @@ from fabric.api import *
 from datetime  import datetime
 import os
 
-env.hosts = ['34.74.237.63']
-env.user = 'ubuntu'
-
+env.hosts = ['34.74.237.63', '54.234.24.109']
 
 def do_pack():
-
     try:
         local("mkdir -p versions")
         date = datetime.now().strftime('%Y%m%d%H%M%S')
@@ -22,15 +19,20 @@ def do_pack():
 
 def do_deploy(archive_path):
 
+    if not(os.path.exists(archive_path)):
+            return False
     try:
         put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/{}".format(m_file[0]))
         f_file = os.path.basename(archive_path)
         m_file = os.path.splitext(f_file)
-        run("tar -C  /data/web_static/releases/{} -xvf /tmp/{}".format(m_file[0], f_file))
-        run("rm /tmp/{}".format(f_file))
-        run("rm -rf /data/web_static/current")
-        run("ln -sf /data/web_static/releases/{} /data/web_static/current".format(m_file[0]))
+        run("sudo mkdir -p /data/web_static/releases/{}".format(m_file[0]))
+        run("sudo tar -C  /data/web_static/releases/{} -xvf /tmp/{}".format(m_file[0], f_file))
+        run("sudo rm /tmp/{}".format(f_file))
+        run("sudo mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}".format(m_file[0], m_file[0]))
+        run("sudo rm -rf /data/web_static/releases/{}/web_static/".format(m_file[0]))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -sf /data/web_static/releases/{} /data/web_static/current".format(m_file[0]))
         return True
-    except:
+    except Exception as e:
+        print(e)
         return False
