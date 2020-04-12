f1c5e57b757b7b4600c031b3db888681aaf495f4
Nesgomez9 <nicolico99@hotmail.com>
2020-04-12 18:23:29 +0000

    [Add] Adding task 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,13 +11,13 @@ def do_deploy(archive_path):
 
     try:
         put(archive_path, "/tmp/")
-        run("mkdir -p /data/web_static/releases/")
+        run("mkdir -p /data/web_static/releases/{}".format(m_file[0]))
         f_file = os.path.basename(archive_path)
         m_file = os.path.splitext(f_file)
-        run("tar -C  /data/web_static/releases/{} -xvf {}".format(m_file, f_file))
+        run("tar -C  /data/web_static/releases/{} -xvf {}".format(m_file[0], f_file))
         run("rm /tmp/{}".format(f_file))
         run("rm -rf /data/web_static/current")
-        run("ln -sf /data/web_static/releases/{} /data/web_static/current".format(m_file))
+        run("ln -sf /data/web_static/releases/{} /data/web_static/current".format(m_file[0]))
         return True
     except:
         return False
