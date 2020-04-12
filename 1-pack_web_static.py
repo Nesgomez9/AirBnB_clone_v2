262a51c395f68e443ae3b2f0b032281df7da3cd5
Nesgomez9 <nicolico99@hotmail.com>
2020-04-12 17:16:38 +0000

    [Add] Add second mandatory task

---
+++ 1-pack_web_static.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from fabric.api import *
+from datetime  import datetime
+
+def do_pack():
+    try:
+        local("mkdir -p versions")
+        date = datetime.now().strftime('%Y%m%d%H%M%S')
+        path = "versions/web_static_{}".format(date)
+        local("tar -cvzf {}.tgz web_static/".
+              format(path))
+        return (path)
+    except:
+        return None
